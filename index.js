a41afec699d5d1ed6be29b00f505a8f16f9d6c28
Bachamak <tarasenkodaniilromanovich@gmail.com>
2023-02-07 22:17:12 +0200

    Код переделанный

--- index.js
+++ index.js
@@ -3,13 +3,10 @@ let askQuestion = prompt("What do you want to ask: +, -, *, / ?");
 let askNumbers = prompt('Write two numbers using commas between them');
 let twoNumbers = askNumbers.split(',');
 
-let oneNumber = twoNumbers[0];
-let twoNumber = twoNumbers[1];
-
-let sumAdd = +oneNumber[0] + +twoNumber[1];
-let sumSub = +oneNumber[0] - +twoNumber[1];
-let sumMult = +oneNumber[0] * +twoNumber[1];
-let sumDiv = +oneNumber[0] / +twoNumber[1];
+let sumAdd = +twoNumbers[0] + +twoNumbers[1];
+let sumSub = +twoNumbers[0] - +twoNumbers[1];
+let sumMult = +twoNumbers[0] * +twoNumbers[1];
+let sumDiv = +twoNumbers[0] / +twoNumbers[1];
 
 while (askQuestion !== '+' && askQuestion !== '-' && askQuestion !== '*' && askQuestion !== '/' && askQuestion !== ' ') {
     alert("sorry, you did not enter any of the options", askQuestion);
@@ -17,11 +14,11 @@ while (askQuestion !== '+' && askQuestion !== '-' && askQuestion !== '*' && askQ
 };
 
 if (askQuestion === '+') {
-    alert(`${oneNumber} + ${twoNumber} = ${sumAdd}`);
+    alert(`${twoNumbers[0]} + ${twoNumbers[1]} = ${sumAdd}`);
 } else if(askQuestion === '-') {
-    alert(`${oneNumber} - ${twoNumber} = ${sumSub}`);
+    alert(`${twoNumbers[0]} - ${twoNumbers[1]} = ${sumSub}`);
 } else if(askQuestion === '*') {
-    alert(`${oneNumber} * ${twoNumber} = ${sumMult}`);
+    alert(`${twoNumbers[0]} * ${twoNumbers[1]} = ${sumMult}`);
 } else if(askQuestion === '/') {
-    alert(`${oneNumber} / ${twoNumber} = ${sumDiv}`);
+    alert(`${twoNumbers[0]} / ${twoNumbers[1]} = ${sumDiv}`);
 };
